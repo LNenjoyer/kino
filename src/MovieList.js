818a35c1c8f16aa7872a0b3232a380aa5cd84db1
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-10-18 13:47:53 +0200

    zmiany1

--- src/MovieList.js
+++ src/MovieList.js
@@ -13,14 +13,14 @@ const Movielist = (props) => (
                 <div className="gatunek">{props.gatunek}</div>
             </div>
 
-            <div className={props.premiera != "" ? 'premiera' : 'nie'}>{props.premiera}</div>
-            <div className={props.salas != "" ? 'salas' : 'nie'}>{props.salas}</div>
-            <div className={props.dldzieci != "" ? 'dldzieci' : 'nie'}>{props.dldzieci}</div>
-            <div className={props.stud != "" ? 'stud' : 'nie'}>{props.stud}</div>
+            <div className={props.premiera !== "" ? 'premiera' : 'nie'}>{props.premiera}</div>
+            <div className={props.salas !== "" ? 'salas' : 'nie'}>{props.salas}</div>
+            <div className={props.dldzieci !== "" ? 'dldzieci' : 'nie'}>{props.dldzieci}</div>
+            <div className={props.stud !== "" ? 'stud' : 'nie'}>{props.stud}</div>
 
         </div>
-        <div className={props.czas1 != "" ? 'czas' : 'nie'}>{props.czas1}</div>
-        <div className={props.czas2 != "" ? 'czas' : 'nie'}>{props.czas2}</div>
+        <div className={props.czas1 !== "" ? 'czas' : 'nie'}>{props.czas1}</div>
+        <div className={props.czas2 !== "" ? 'czas' : 'nie'}>{props.czas2}</div>
     </div>
 );
 export default Movielist;
